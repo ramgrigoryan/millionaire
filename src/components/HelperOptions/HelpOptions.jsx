02a3eb03261f731ce3bf20193478e131d0d317ac
= <=>
2022-08-01 10:47:39 +0400

    act & hide for helper

--- src/components/HelperOptions/HelpOptions.jsx
+++ src/components/HelperOptions/HelpOptions.jsx
@@ -1,11 +1,15 @@
 import { HelperButton, HelpersContainer } from "./helpoptions.style";
 import { fiftyFifty } from "../../features/gameSlice/gameSlice";
 import { useDispatch } from "react-redux";
+import { useState } from "react";
 const HelpOptions = () => {
+  const [showFifty,setShowFifty] =useState(true);
   const dispatch = useDispatch();
   return (
     <HelpersContainer>
-      <HelperButton onClick={()=>dispatch(fiftyFifty())}>50/50</HelperButton>
+      <HelperButton onClick={()=>{
+        setShowFifty(false);
+        dispatch(fiftyFifty())}} style={!showFifty?{"visibility":"hidden"}:{"visibility":"display"}}>50/50</HelperButton>
       <HelperButton>Audience</HelperButton>
       <HelperButton>Call a friend</HelperButton>
     </HelpersContainer>
